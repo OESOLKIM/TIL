28a72ee81add7f768e4abee09824a8abb1b767f3
Oesol Kim <56331160+wesley-94@users.noreply.github.com>
2021-02-22 16:28:47 +0900

    Add files via upload

--- java_basics/Car.java
+++ java_basics/Car.java
@@ -1,5 +1,7 @@
-package javaStudy;
+package javaStudy_01;
 
 public class Car {
-
+	public void run() {
+		System.out.println("Car의 run 메소드");
+	}
 }
--- java_basics/CarExam.java
+++ java_basics/CarExam.java
@@ -1,4 +1,4 @@
-package javaStudy;
+package javaStudy_01;
 
 public class CarExam {
 
@@ -6,7 +6,17 @@ public class CarExam {
 		Car c1 = new Car();
 		Car c2 = new Car();
 		
+		c1.name = "소방차";
+		c1.number = 1234;
 		
+		c2.name = "구급차";
+		c2.number = 1111;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
+		
+		System.out.println(c2.name);
+		System.out.println(c2.number);
 	}
 
 }
